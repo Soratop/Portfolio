d031fa4310b98bef36a7788799e1ba930cc99631
Soratop <65211140+Soratop@users.noreply.github.com>
2023-11-22 18:04:30 +0900

    Update

--- Portfolio.py
+++ Portfolio.py
@@ -5,6 +5,7 @@ import pandas as pd
 import datetime
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Portfolio list
 # USD
@@ -17,12 +18,24 @@ JP_tickers=["^N225"]
 
 #Time
 Today = pd.to_datetime(datetime.date.today())
+Start_time="2015-01-01"
+Total_days=(Today-pd.to_datetime(Start_time)).days
 Today= Today.strftime('%Y-%m-%d')
 
 #Get deta
-df = yf.download(USD_tickers,start="2010-01-01", end=Today, interval = "1d")
+df = yf.download(USD_tickers,start=Start_time, end=Today, interval = "1d")
 
-# Compute pairwise correlation  
+# Check for missing values.
+print("Count the Nan")
+print(df.isnull().sum())
+
+# Interpolation
+df=df.interpolate()
+print("After interpolating")
+print(df.isnull().sum())
+
+# Compute pairwise correlation
+Df_adj_close=df["Adj Close"]
 Df_corr = df["Adj Close"].corr().round(3)
 
 # shown graph
@@ -30,7 +43,17 @@ fig, ax = plt.subplots(figsize=(12, 9))
 sns.heatmap(Df_corr,cmap="Reds",vmin=0, vmax=1,annot=True)
 plt.savefig('correlation.png')
 
-Data=Backtest(tickers= USD_tickers,start="2010-01-01",end=Today).run()
+# Day return
+Day_return=1+Df_adj_close.pct_change()
+
+# cumulative sum
+cum_return=(1+Df_adj_close.pct_change()).cumprod()
+
+cum_return.plot(figsize=(12,8))
+plt.savefig('Returns.png')
+#plt.show()
+
+Data=Backtest(tickers= USD_tickers,start="2015-01-01",end=Today).run()
 
 # setting Advanced Run(True or False)
 Advanced_run = False
