09d3c1cd31f3a1b51385a7cdb3891cc1b8ac33bb
Soratop <65211140+Soratop@users.noreply.github.com>
2023-12-08 21:58:10 +0900

    test

--- html/test.py
+++ html/test.py
@@ -7,9 +7,8 @@ from pyodide.http import open_url
 
 url = "https://raw.githubusercontent.com/Soratop/Portfolio/main/html/Data/df_usd.csv"
 df_usd = pd.read_csv(open_url(url))
-display(df_usd, target="result")
 df_corr= df_usd.corr().round(3)
 fig, ax = plt.subplots(figsize=(12, 9))
 sns.heatmap(df_corr,cmap="Reds",vmin=0, vmax=1,annot=True)
-#display(fig, target="result")
+display(fig, target="result")
 
