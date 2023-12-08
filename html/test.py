eea1e6926025120cf900888f2abadbc564c5079e
Soratop <65211140+Soratop@users.noreply.github.com>
2023-12-08 21:54:43 +0900

    test

--- html/test.py
+++ html/test.py
@@ -3,41 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from pyodide.http import open_url
+     
 
-def show_heatmap(Data,saveplace,show=False,save=False):
-    if show:
-        fig, ax = plt.subplots(figsize=(12, 9)) 
-        sns.heatmap(Data,cmap="Reds",vmin=0, vmax=1,annot=True)
-    if save==True:
-        plt.savefig(saveplace)
-    if show:
-        plt.show()
-        
-def set_data():
-    url = "https://raw.githubusercontent.com/Soratop/Portfolio/main/html/Data/Japan_date.csv"
-    voo_date = pd.read_csv(open_url(url))
-    #voo_date=pd.read_csv("D:\Portfolio\html\Data\Voo_date.csv")
-    
-    url = "https://raw.githubusercontent.com/Soratop/Portfolio/main/html/Data/Qqq_date.csv"
-    qqq_date = pd.read_csv(open_url(url))
-    #qqq_date=pd.read_csv("html\Data\Qqq_date.csv")
-    
-    url = "https://raw.githubusercontent.com/Soratop/Portfolio/main/html/Data/Jpy_x_date.csv"
-    jpy_x_date = pd.read_csv(open_url(url))
-    
-    #jpy_x_date=pd.read_csv("html\Data\Jpy_x_date.csv")
-    
-    url = "https://raw.githubusercontent.com/Soratop/Portfolio/main/html/Data/Japan_date.csv"
-    n225_date = pd.read_csv(open_url(url))
-    
-    #n225_date=pd.read_csv("html\Data\Japan_date.csv")
-    
-    n225_date["Usd Close"]=0
-    for i in range(len(n225_date)):
-        n225_date["Usd Close"][i]=n225_date["Adj Close"][i]/jpy_x_date["Adj Close"][i]
-    df_usd=pd.DataFrame({"USDJPY":jpy_x_date["Adj Close"],"VOO":voo_date["Adj Close"],"QQQ":qqq_date["Adj Close"],"N225":n225_date["Usd Close"]})
-    df_corr= df_usd.corr().round(3)
-    show_heatmap(show=True,Data=df_corr,saveplace='Swap_graph/correlation.png')
-    
-print(set_data())
+url = "https://raw.githubusercontent.com/Soratop/Portfolio/main/html/Data/df_usd.csv"
+df_usd = pd.read_csv(open_url(url))
+display(df_usd, target="result")
+df_corr= df_usd.corr().round(3)
+fig, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(df_corr,cmap="Reds",vmin=0, vmax=1,annot=True)
+#display(fig, target="result")
 
